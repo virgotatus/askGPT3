ff3e0dafdbe463a91285ae7d21025103e2f08a5f
virgotatus <435491723@qq.com>
2023-02-26 11:42:29 +0800

    add jQuery lib in javascript;fix favicon

--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -3,6 +3,6 @@ class Prompt < ApplicationRecord
   has_one :detail
   accepts_nested_attributes_for :detail, allow_destroy: true
 
-  validates :title, presence: true
-  validates :body, presence: true
+  validates :title, presence: true, length: { minimum: 1 }
+  validates :body, presence: true, length: { minimum: 5 }
 end
