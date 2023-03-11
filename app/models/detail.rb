fd3be20f70b76185caa9c325aaec949fa08b8a54
virgotatus <435491723@qq.com>
2023-03-11 12:30:53 +0800

    refine Readme.md and some comments, annotations, bugs

--- app/models/detail.rb
+++ app/models/detail.rb
@@ -1,3 +1,4 @@
 class Detail < ApplicationRecord
+  # prompts's detail like img name and img_link, alter questions, description
   belongs_to :prompt
 end
--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,3 +1,4 @@
 class Reply < ApplicationRecord
+  # reply of GPT , including answer and audio_url by resemble.ai
   belongs_to :prompt
 end
